ef05b573f990728dd9390427ff210e3d8920f411
Gabriel Braun Rodenbusch <allesbraun123@gmail.com>
2023-10-22 02:44:00 -0300

    secret key protection

--- setup/settings.py
+++ setup/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,12 +26,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-havvvpkz!h6-v+ee+lne8m6in+p398xmw=#uctm3sixqr=@uh8'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['rtcc-40c886516567.herokuapp.com']
+ALLOWED_HOSTS = ['rtcc-40c886516567.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
