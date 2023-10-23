2a229cc9ac171641f585668e0f23bb65c3b9ee6f
Gabriel Braun Rodenbusch <allesbraun123@gmail.com>
2023-10-23 18:41:45 -0300

    views and urls simplified

--- data/views.py
+++ data/views.py
@@ -21,6 +21,8 @@ class CodesViewSet(viewsets.ModelViewSet):
     def create(self, request, *args, **kwargs):
         response = super().create(request, *args, **kwargs)
         uploaded_file = self.request.data.get('file')
+        if 'file' not in request.FILES:
+            return Response({'error': 'File not sent'}, status=status.HTTP_400_BAD_REQUEST)
         # Verifica se a extensão do arquivo é .java
         if not uploaded_file.name.endswith('.java'):
                 return Response({'error': 'The file must have a .java extension'}, status=status.HTTP_400_BAD_REQUEST)
@@ -40,65 +42,52 @@ class CodesViewSet(viewsets.ModelViewSet):
         return response
         
     
-class JavaFileViewSet(APIView):  # Use a classe APIView ao invés de ViewSet
-    def get(self, request, filename=None):  # Use o método 'get' ao invés de 'retrieve'
-        if request.method == 'GET':
-            queryset = Code.objects.all()
-            if filename is not None:
-                # Verifica se o arquivo Java existe no diretório 'java_files'
-                java_file_path = os.path.join(settings.MEDIA_ROOT, filename)
-                if os.path.exists(java_file_path):
-                    # Se o arquivo existir, retorna-o como uma resposta HTTP
-                    with open(java_file_path, 'rb') as file:                      
-                        #o download acontece por causa das linhas de baixo
-                        response = HttpResponse(file)
-                        response['Content-Type'] = 'application/octet-stream'
-                        response['Content-Disposition'] = f'attachment; filename="{filename}"'
-                        return response                  
-                else:
-                    # Se o arquivo não existir, retorna uma resposta 404
-                    return Response(status=status.HTTP_404_NOT_FOUND)
-            else:
-                java_files_dir = settings.MEDIA_ROOT
-                files = os.listdir(java_files_dir)
-                return Response({"files": files})
+# class JavaFileViewSet(APIView):  # Use a classe APIView ao invés de ViewSet
+#     def get(self, request, filename=None):  # Use o método 'get' ao invés de 'retrieve'
+#         if request.method == 'GET':
+#             if filename is not None:
+#                 # Verifica se o arquivo Java existe no diretório 'java_files'
+#                 java_file_path = os.path.join(settings.MEDIA_ROOT, filename)
+#                 if os.path.exists(java_file_path):
+#                     # Se o arquivo existir, retorna-o como uma resposta HTTP
+#                     with open(java_file_path, 'rb') as file:                      
+#                         #o download acontece por causa das linhas de baixo
+#                         response = HttpResponse(file)
+#                         response['Content-Type'] = 'application/octet-stream'
+#                         response['Content-Disposition'] = f'attachment; filename="{filename}"'
+#                         return response                  
+#                 else:
+#                     # Se o arquivo não existir, retorna uma resposta 404
+#                     return Response(status=status.HTTP_404_NOT_FOUND)
+#             else:
+#                 java_files_dir = settings.MEDIA_ROOT
+#                 files = os.listdir(java_files_dir)
+#                 return Response({"files": files})
             
-    def post(self, request, filename = None):
-        if request.method == 'POST':
-            queryset = Code.objects.all()
-            # return render(request, 'data/templates/upload_form.html')
-            if 'file' not in request.FILES:
-                return Response({"error": "Arquivo não enviado"}, status=status.HTTP_400_BAD_REQUEST)
-            # Verifica se a extensão do arquivo é .java
-            uploaded_file = request.FILES['file']
-            if not uploaded_file.name.endswith('.java'):
-                return Response({'error': 'O arquivo deve ter extensão .java'}, status=status.HTTP_400_BAD_REQUEST)
-            # Verifica o tamanho do arquivo
-            max_size = 1024 * 1024  # Tamanho máximo permitido em bytes (1 MB)
-            if uploaded_file.size > max_size:
-                return Response({'error': 'O arquivo é muito grande. O tamanho máximo permitido é 1MB.'}, status=status.HTTP_400_BAD_REQUEST)
-            
-            content = uploaded_file.read().decode('utf-8')
+#     def post(self, request, filename = None):
+#         if request.method == 'POST':            
+#             uploaded_file = request.FILES['file']
+#             content = uploaded_file.read().decode('utf-8')
 
-            # Verificar se a pasta 'java_files' já existe
-            if not os.path.exists(settings.MEDIA_ROOT):
-                # Se não existir, criar a pasta
-                os.makedirs(settings.MEDIA_ROOT)
-            # Salvar o arquivo no diretório 'java_files'
-            file_path = os.path.join(settings.MEDIA_ROOT, uploaded_file.name)
-            with open(file_path, 'wb') as file:
-                # file.write(uploaded_file.read())
-                file.write(content.encode('utf-8'))
+#             # Verificar se a pasta 'java_files' já existe
+#             if not os.path.exists(settings.MEDIA_ROOT):
+#                 # Se não existir, criar a pasta
+#                 os.makedirs(settings.MEDIA_ROOT)
+#             # Salvar o arquivo no diretório 'java_files'
+#             file_path = os.path.join(settings.MEDIA_ROOT, uploaded_file.name)
+#             with open(file_path, 'wb') as file:
+#                 # file.write(uploaded_file.read())
+#                 file.write(content.encode('utf-8'))
                 
-            # Armazena a eficiência e a complexidade do código em um dicionário
-            response_data = data_response(content, uploaded_file)
-            result_json = json.loads(response_data)  # Converte a string JSON de volta para um dicionário Python
+#             # Armazena a eficiência e a complexidade do código em um dicionário
+#             response_data = data_response(content, uploaded_file)
+#             result_json = json.loads(response_data)  # Converte a string JSON de volta para um dicionário Python
 
-            efficiency = result_json['Efficiency']
-            complexity_class = result_json['Complexity class']
+#             efficiency = result_json['Efficiency']
+#             complexity_class = result_json['Complexity class']
 
-            # Constrói a string de resposta
-            response_text = f'Efficiency: {efficiency}, Complexity class: {complexity_class}'
+#             # Constrói a string de resposta
+#             response_text = f'Efficiency: {efficiency}, Complexity class: {complexity_class}'
 
-            return HttpResponse(response_text, content_type="text/plain")
+#             return HttpResponse(response_text, content_type="text/plain")
             
--- setup/urls.py
+++ setup/urls.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 
-from data.views import CodesViewSet, JavaFileViewSet
+from data.views import CodesViewSet
+
+# , JavaFileViewSet
 
 router = routers.DefaultRouter()
 router.register('codes', CodesViewSet, basename='Codes')
@@ -14,7 +16,7 @@ urlpatterns = [
     # path('general-control/', admin.site.urls),
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('java_files/<str:filename>/', JavaFileViewSet.as_view(), name='java_files'),
-    path('java_files/', JavaFileViewSet.as_view(), name='java_files_list'),
-    path('<str:filename>/', JavaFileViewSet.as_view(), name='java_files'),
+#     path('java_files/<str:filename>/', JavaFileViewSet.as_view(), name='java_files'),
+#     path('java_files/', JavaFileViewSet.as_view(), name='java_files_list'),
+#     path('<str:filename>/', JavaFileViewSet.as_view(), name='java_files'),
 ]
