b483923d86dca8815ff43d98174a9706fa3565d3
Gabriel Braun Rodenbusch <allesbraun123@gmail.com>
2023-11-03 13:49:40 -0300

    models

--- data/models.py
+++ data/models.py
@@ -6,12 +6,11 @@ from django.db import models
 
 class Code(models.Model):
     file = models.FileField()
-    title = models.CharField(max_length=100, default = 'main')  # mudar para blank=True em vez de default='main'?
-    # code_description = models.CharField(max_length=500, default = 'A java code.')
-
+    title = models.CharField(max_length=100, blank = True) 
+    
     def save(self, *args, **kwargs):
         # Se o título ainda não foi definido, gera-o com base no nome do arquivo
-        if self.title == 'main':
+        if not self.title:
             file_name = os.path.basename(self.file.name)
             self.title = os.path.splitext(file_name)[0]
         super(Code, self).save(*args, **kwargs)
