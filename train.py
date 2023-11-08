b78a2c0f8dc22d901710f8e84b6ef254c9cf46ba
Gabriel Braun Rodenbusch <allesbraun123@gmail.com>
2023-11-08 20:02:07 -0300

    test xgboost

--- train.py
+++ train.py
@@ -8,19 +8,9 @@ path_merged = 'databases/mergeds.csv'
 train_data = pd.read_csv(path_merged)
 train_data_efficiency = train_data.drop(['complexity_class'], axis=1)
 train_data_class = train_data
-# train_data_both = train_data.drop(['complexity'], axis=1) 
 
-# custom_metrics_class = {'accuracy', 'precision', 'f1', 'recall'}
-# custom_metrics_efficiency = {'accuracy', 'precision', 'f1', 'recall', 'roc_auc'}  
-
-# predictor = TabularPredictor(label='complexity_class', eval_metric = custom_metrics_class ).fit(train_data_class,verbosity  = 4, presets=['best_quality'])
-# predictor = TabularPredictor(label='efficiency', eval_metric = custom_metrics_efficiency ).fit(train_data_efficiency,verbosity  = 4, presets=['best_quality'])
 predictor = TabularPredictor(label='efficiency').fit(train_data_efficiency, presets=['medium_quality', 'optimize_for_deployment'])
 predictor = TabularPredictor(label='complexity_class').fit(train_data_class, presets=['medium_quality', 'optimize_for_deployment'])
-# predictor = TabularPredictor(label=['complexity_class', 'efficiency']).fit(train_data_both,verbosity  = 4, presets=['best_quality'])
-
-#0.6909   = Validation score   (accuracy) #class primeiro
-#0.9922 efficiency segundo
 
 # Ensemble indices: [3]
 # Ensemble weights: 
